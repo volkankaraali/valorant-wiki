11e7b9d5adce686bfbed5a315c0da05ca4aea9fb
volkan karaali <volkankaraali1@gmail.com>
2022-01-31 23:39:03 +0300

    footer added

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+//css import
+import styles from "./footer.module.css"
 function Footer() {
-    return <div>footer</div>;
+    return (
+        <footer >
+            <a rel="noreferrer" href="https://github.com/volkankaraali" target="_blank">github/volkankaraali</a>
+            <a rel="noreferrer" href="https://valorant-api.com/" target="_blank">valorant api</a>
+        </footer>
+    );
 }
 
 export default Footer;
